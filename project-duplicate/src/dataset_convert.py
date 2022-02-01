12ba985f1bfac74a4c4ca6324b2b4d6868ec2e99
GhastCMD <kraken32bin@protonmail.com>
2022-02-01 17:01:28 -0300

    Added tqdm
    Corrected bug with arguments of conv_test_file

--- project-duplicate/src/dataset_convert.py
+++ project-duplicate/src/dataset_convert.py
@@ -3,13 +3,14 @@ import PIL.Image as pil_image
 import h5py
 import numpy as np
 import argparse
+from tqdm import tqdm
 
 def conv_train_file(dataset_input, dataset_output):
     high_res_images: list = []
     low_res_images: list = []
     
     with h5py.File(dataset_input, 'r') as hdf:
-        for image in hdf['hr']:
+        for image in tqdm(hdf['hr']):
             out_img = pil_image.fromarray(image).convert('RGB')
             
             out_img = out_img.resize(((out_img.width // 2) * 2, (out_img.height // 2) * 2))
@@ -26,8 +27,8 @@ def conv_test_file(dataset_input, dataset_output):
     high_res_images = {}
     low_res_images = {}
 
-    with h5py.File('./dataset/Set5_x2.h5', 'r') as hdf:
-        for index in [str(x) for x in range(5)]:
+    with h5py.File(dataset_input, 'r') as hdf:
+        for index in tqdm([str(x) for x in range(5)]):
             image = hdf['hr'][index][:,:]
             
             out_shape = image.shape
@@ -39,7 +40,7 @@ def conv_test_file(dataset_input, dataset_output):
             out_img = out_img.resize((out_img.width // 2, out_img.height // 2))
             low_res_images[index] = np.array(out_img.convert('F')).reshape((out_shape[0]//2,out_shape[1]//2))
         
-    with h5py.File('./output/Set5_x2_converted.h5', 'w') as hdf:
+    with h5py.File(dataset_output, 'w') as hdf:
         lr_imgs = hdf.create_group('lr')
         hr_imgs = hdf.create_group('hr')
         for index in [str(x) for x in range(5)]:
