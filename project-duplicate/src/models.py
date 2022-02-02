bfe9e8afdec35d67a2502afc5b818f2ee5a22c35
GhastCMD <kraken32bin@protonmail.com>
2022-02-02 17:05:47 -0300

    Added final touches for comparison: with and without improvement

--- project-duplicate/src/models.py
+++ project-duplicate/src/models.py
@@ -16,3 +16,17 @@ class SRCNN(nn.Module):
         x = self.relu(self.conv3(x))
         x = self.deconv(x)
         return x
+
+class SRCNN_old(nn.Module):
+    def __init__(self, num_channels=1):
+        super(SRCNN_old, self).__init__()
+        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=9, padding=9 // 2)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=5 // 2)
+        self.conv3 = nn.Conv2d(32, num_channels, kernel_size=5, padding=5 // 2)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.conv3(x)
+        return x
